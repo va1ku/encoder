771874e4ca5e01ba76e2ed9d911ed7bf46728bee
Vasili Nadachkovski <nadachkovckivacili@gmail.com>
2022-04-02 17:27:18 +0500

    last front-v

--- sourse/js/index.js
+++ sourse/js/index.js
@@ -1,7 +1,26 @@
 "use strict";
 
 
-document.addEventListener("DOMContentLoaded", function() {
-	window.scroll( 0 , 50 );
-	console.log("da")
+const text = document.querySelector("textarea");
+const but = document.querySelector("button");
+const form = document.querySelector(".encoder__form")
+
+text.addEventListener("keyup" , (event) =>{
+	if (text.value.length >= 1){
+		but.classList.add("active");
+	}else{
+		but.classList.remove("active");
+	}
+})
+
+text.addEventListener("keydown" , (event) =>{
+	console.log(event.key )
+	if(event.key == "Enter" && event.ctrlKey){
+		text.value = text.value + "\n";
+	}else if(event.key == "Shift" && event.ctrlKey ){
+		text.value = text.value + "command text"
+	}else if(event.key == "Enter"){
+		event.preventDefault()
+		form.submit()
+	}
 })
